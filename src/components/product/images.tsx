62c330f80224b24b0b00e1ccc6f9fbb53e33f6e9
dima <polker17@mail.ru>
2022-10-12 19:54:15 +0300

    фото продукта

--- src/components/product/images.tsx
+++ src/components/product/images.tsx
@@ -2,30 +2,38 @@ import React, {useState} from 'react';
 import styled from "styled-components";
 import {BASE_URL} from "src/utils/constants";
 
-const Main = styled.div`
-  position: relative;
-  margin: 0 30px 30px 30px;
-  display: grid;
-  align-content: center;
-  justify-items: center;
+const Main = styled.div<{src: string}>`
+  background-image: url(${props => props.src});
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
   
-  & img {
-    max-height: 400px;
-    max-width: 100%;
-  }
+  position: relative;
+  margin: 30px;
+
+  height: calc(100% - 60px);
+  max-height: 400px;
   
   .mobile & {
     margin: 0;
+  }
+  
+  &.noImage {
+    width: 100%;
+    background-color: rgba(196,196,196,0.23);
   }
 `;
 
 const Other = styled.div`
   position: relative;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  padding: 0 30px;
+  display: flex;
+  //grid-template-columns: 1fr 1fr 1fr;
+  //padding: 0 80px;
   grid-gap: 16px; gap: 16px;
-  grid-auto-rows: 1fr;
+  //grid-auto-rows: 1fr;
+  height: 100px;
+  margin: 30px;
+  flex-wrap: wrap;
   
   .mobile & {
     padding: 0
@@ -36,23 +44,25 @@ const ImageStyle = styled.div<{src: string}>`
   cursor: pointer;
   background-image: url(${props => props.src});
   background-position: center;
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
   aspect-ratio: 1;
+  border: 2px solid white;
+  background-color: white;
+  border-radius: 10px;
+  height: 100%;
+  
+  &.selected {
+    border-color: #2196F3;
+  }
 `;
 
-const NoImage = styled.div`
-  height: 400px;
-  width: 100%;
-  background-color: rgba(196,196,196,0.23);
-`;
-
-const Image = (props: {src: string; self: number; func: (i: number) => void}) => {
+const Image = (props: {src: string; self: number; func: (i: number) => void; selected: boolean}) => {
     function click() {
         props.func(props.self);
     }
     return(
-        <ImageStyle src={BASE_URL + props.src} onClick={click} />
+        <ImageStyle className={props.selected ? 'selected' : undefined} src={BASE_URL + props.src} onClick={click} />
     )
 }
 
@@ -65,17 +75,11 @@ const Images = (props: {images: string[]}) => {
 
     return (
         <div>
-            <Main>
-                {
-                    props.images[selected] ?
-                        <img src={BASE_URL + props.images[selected]} />
-                        :
-                        <NoImage />
-                }
-            </Main>
+            <Main src={props.images[selected] ? BASE_URL + props.images[selected] : ''}
+                  className={props.images[selected] ? '' : 'noImage'} />
             <Other>
                 { props.images.map((image, i) =>
-                    <Image key={i} func={select} self={i} src={image} />
+                    <Image selected={selected == i} key={i} func={select} self={i} src={image} />
                 ) }
             </Other>
         </div>
